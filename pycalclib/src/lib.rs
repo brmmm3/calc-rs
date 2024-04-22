f7ed3eb7de7ea96ca9230e3b550c82fcaa60196e
Martin Bammer <mrbm74@gmail.com>
2024-04-22 05:36:54 +0200

    Update comment

--- pycalclib/src/lib.rs
+++ pycalclib/src/lib.rs
@@ -2,10 +2,7 @@ use pyo3::prelude::*;
 
 mod calc;
 
-/// scandir_rs is a directory iteration module like os.walk(), but with more features and higher speed. Depending on the function call
-/// it yields a list of paths, tuple of lists grouped by their entry type or DirEntry objects that include file type and stat information along
-/// with the name. Using scandir_rs is about 2-17 times faster than os.walk() (depending on the platform, file system and file tree structure)
-/// by parallelizing the iteration in background.
+/// pycalclib_rs is a simple example for using Rust to create Python extension modules.
 #[pymodule]
 #[pyo3(name = "pycalclib_rs")]
 fn init(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
